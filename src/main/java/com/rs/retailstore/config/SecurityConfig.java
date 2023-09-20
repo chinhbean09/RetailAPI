e4b7090fdcb33adb2f11d8a5a6f8e1dbf5d6f425
chinhbean09 <chinh0726@gmail.com>
2023-09-20 15:14:59 +0700

    SpringSecurity-001

---
+++ src/main/java/com/rs/retailstore/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.rs.retailstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration //de spring paste qua file class va no se define những config trong đây
+                // de no biet bean cua chung ta la SecurityConfig, va no se tim trong SecurityConfig nhung cai bean nhu userDetailsService va InMemoryUserDetailsManager
+public class SecurityConfig { //spring security se su dung thog tin trong bean de no co the xac thuc cho chung ta
+    @Bean //cho spring biet minh dang khoi tao bean và ta sẽ bỏ dữ liệu vào đây
+    public UserDetailsService userDetailsService(){
+        //sử dụng User.builder() để tạo một đối tượng UserDetails.
+        UserDetails user = User.builder()
+                .username("chinhdo1")
+                .password("{bcrypt}$2a$10$UHU/QY3Fi0Fl/nivEGMlJeq021R5Xg0qoYWJjea4Ho69c6fcH3rRC")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("thaole")
+                .password("{bcrypt}$2a$10$kkzxjEKmD9exPtbBtlYInO..CBQWigRb.wlEvd2TAfYQ8Xj0zDQxK")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+
+    }
+
+}
